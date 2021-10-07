8e53540624d8e40a0fd199fc43c4c2516d7494f6
Nandhu <codezero2k18@gmail.com>
2021-10-07 17:51:46 +0530

    https://github.com/Nandhu-777/reg-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+constructor(props) {
+    super(props);
+    this.state={
+           
+    };
 }
 
-export default App;
+
+
+
+componentDidMount() {
+         // get all entities - GET
+         fetch("https://apidev.veehive.ai/v1/auth/register", {
+           "method": "POST",
+           "headers": {
+
+                "Basic-Auth": "Veehive VmVlaGl2ZURldjpWIyNIIXZlRDN2IQ=="
+                
+           }
+         })
+        .then(response => response.json())
+         .then(response => {
+           this.setState({
+             signup: response
+           })
+         })
+         .catch(err => { console.log(err); 
+         });
+       }
+}
+
+
+
+
+export {App};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,390 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM  from 'react-dom';
+import FadeTransition from "./transitions/fadeTransitions";
+import "../src/react.css";
+class App extends React.Component{
+   
+  constructor(props){
+    super(props);
+    this.state={
+      isEmailOpen:true,
+      isMobileOpen:false
+    };
+  }
+  
+  showEmailBox(){
+    this.setState({isEmailOpen: true, isMobileOpen: false});
+  }
+  
+  showMobileBox(){
+    this.setState({isMobileOpen:true, isEmailOpen: false});
+  }
+  
+  render(){
+         return(
+           <div className="root-container">
+            <div className="box-controller">
+             <div
+                 className={"controller " + (this.state.isEmailOpen
+                  ? "selected-controller"
+                  : "")}
+                  onClick={this
+                  .showEmailBox
+                  .bind(this)}>
+                  Email</div>
+             <div 
+                className={"controller " + (this.state.isMobileOpen
+                  ? "selected-controller"
+                  : "")}
+                  onClick={this
+                  .showMobileBox
+                  .bind(this)}>
+                  Mobile</div>
+             </div>
+             <FadeTransition isOpen={this.state.isEmailOpen} duration={500}>
+          <div className="box-container">
+            <EmailBox/>
+          </div>
+        </FadeTransition>
+        <FadeTransition isOpen={this.state.isMobileOpen} duration={500}>
+          <div className="box-container">
+            <MobileBox/>
+          </div>
+        </FadeTransition>
+  
+  
+  
+             </div>
+  
+         );
+  
+  
+  }
+  
+  }
+  
+  class EmailBox extends React.Component {
+  
+    constructor(props){
+      super(props);
+      this.state={
+        email: "",
+        username: "",
+        password: "",
+        errors: [],
+        pwdlength: null
+      };
+      
+    }
+    showValidationErr(elm, msg) {
+      this.setState((prevState) => ({
+        errors: [
+          ...prevState.errors, {
+            elm,
+            msg
+          }
+        ]
+      }));
+    }
+    clearValidationErr(elm) {
+      this.setState((prevState) => {
+        let newArr = [];
+        for (let err of prevState.errors) {
+          if (elm != err.elm) {
+            newArr.push(err);
+          }
+        }
+        return {errors: newArr};
+      });
+    }
+    onEmailChange(e) {
+      this.setState({email: e.target.value});
+      this.clearValidationErr("email");
+    }
+  
+    onUsernameChange(e) {
+      this.setState({username: e.target.value});
+      this.clearValidationErr("username");
+    }
+  
+    onPasswordChange(e) {
+      this.setState({password: e.target.value});
+      this.clearValidationErr("password");
+    
+  }
+  
+    submitEmail(e) {
+      console.log(this.state);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    if (this.state.email == "") {
+       this.showValidationErr("email", "Email Cannot be empty!");
+    }
+    if (this.state.username == "") {
+       this.showValidationErr("username", "Username Cannot be empty!");
+    }
+    if (this.state.password == "") {
+       this.showValidationErr("password", "Password Cannot be empty!");
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  }
+
+  
+    render(){
+      let emailErr = null,
+      usernameErr = null,
+      passwordErr = null;
+
+    for (let err of this.state.errors) {
+      if (err.elm == "email") {
+        emailErr = err.msg;
+      }
+      if (err.elm == "username") {
+        usernameErr = err.msg;
+      }
+      if (err.elm == "password") {
+        passwordErr = err.msg;
+      }
+    }
+
+
+
+
+    
+  
+ 
+    return(
+      <div className="inner-container">
+    
+      <div className="box">
+      <div className="input-group">
+      <input 
+      type="email"
+       className="login-input" 
+       name="email" 
+       placeholder="Enter the email" 
+       onChange={this
+       .onEmailChange
+       .bind(this)}/>
+         <small className="danger-error">{emailErr
+               ? emailErr
+               : ""}</small>
+      </div>
+      <div className="input-group">
+      <input 
+      type="text" 
+      className="login-input"
+       name="username" 
+       placeholder="username" 
+       onChange={this
+         .onUsernameChange
+         .bind(this)}/>
+       <small className="danger-error">{usernameErr
+           ? usernameErr
+           : ""}</small>
+      </div>
+      <div className="input-group">
+      <input 
+      type="password" 
+      className="login-input"
+       name="password"
+        placeholder="password"
+        onChange={this
+         .onPasswordChange
+         .bind(this)}/>
+       <small className="danger-error">{passwordErr
+           ? passwordErr
+           : ""}</small> 
+      </div>
+       </div>
+       <div className="group">
+         <input type="checkbox" className="checkbox" checked/> I agree to Food community's T&C 
+         </div>
+       <div>
+       <button 
+       type="button" 
+       className="signup"
+         onClick={this
+         .submitEmail
+         .bind(this)}>Signup</button>
+        <p>or</p>
+      
+       <button type="button" className="login" name="login">Log in</button>
+       </div>
+   
+    
+        </div>
+ 
+       
+   );
+ }
+ 
+ 
+ }
+ 
+  
+       
+  
+  class MobileBox extends React.Component {
+  
+    constructor(props){
+      super(props);
+      this.state={
+        number: "",
+        username: "",
+        password: "",
+        errors: [],
+        pwdlength: null
+    };
+    }
+    showValidationErr(elm, msg) {
+      this.setState((prevState) => ({
+        errors: [
+          ...prevState.errors, {
+            elm,
+            msg
+          }
+        ]
+      }));
+    }
+    clearValidationErr(elm) {
+      this.setState((prevState) => {
+        let newArr = [];
+        for (let err of prevState.errors) {
+          if (elm != err.elm) {
+            newArr.push(err);
+          }
+        }
+        return {errors: newArr};
+      });
+    }
+    onNumberChange(e) {
+      this.setState({number: e.target.value});
+      this.clearValidationErr("number");
+    }
+  
+    onUsernameChange(e) {
+      this.setState({username: e.target.value});
+      this.clearValidationErr("username");
+    }
+  
+    onPasswordChange(e) {
+      this.setState({password: e.target.value});
+      this.clearValidationErr("password");
+      
+    }
+  submitMobile(e) {
+
+    console.log(this.state);
+
+    if (this.state.number == "") {
+      this.showValidationErr("number", "Number Cannot be empty!");
+    }
+    if (this.state.username == "") {
+      this.showValidationErr("username", "Username Cannot be empty!");
+    }
+    if (this.state.password == "") {
+      this.showValidationErr("password", "Password Cannot be empty!");
+    }
+
+  }
+
+  
+    render(){
+      let numberErr = null,
+      usernameErr = null,
+      passwordErr = null;
+
+    for (let err of this.state.errors) {
+      if (err.elm == "number") {
+        numberErr = err.msg;
+      }
+      if (err.elm == "username") {
+        usernameErr = err.msg;
+      }
+      if (err.elm == "password") {
+        passwordErr = err.msg;
+      }
+    }
+      
+    return(
+      
+      <div className="inner-container">
+    
+       <div className="box">
+       <div className="input-group">
+       <input 
+       type="text"
+        className="login-input" 
+        name="number" 
+        placeholder="mobile number" 
+        onValueChange= {this.state.countryCode}
+        onChange={this.onNumberChange.bind(this)}/>
+          <small className="danger-error">{numberErr
+                ? numberErr
+                : ""}</small>
+       </div>
+       <div className="input-group">
+       <input 
+       type="text" 
+       className="login-input"
+        name="username" 
+        placeholder="username" 
+        onChange={this
+          .onUsernameChange
+          .bind(this)}/>
+        <small className="danger-error">{usernameErr
+            ? usernameErr
+            : ""}</small>
+       </div>
+       <div className="input-group">
+       <input 
+       type="password" 
+       className="login-input"
+        name="password"
+         placeholder="password"
+         onChange={this
+          .onPasswordChange
+          .bind(this)}/>
+        <small className="danger-error">{passwordErr
+            ? passwordErr
+            : ""}</small> 
+       </div>
+        </div>
+        <div className="group">
+          <input type="checkbox" className="checkbox" checked/> I agree to Food community's T&C 
+          </div>
+        <div>
+        <button 
+        type="button" 
+        className="signup"
+          onClick={this
+          .submitMobile
+          .bind(this)}>Signup</button>
+         <p>or</p>
+       
+        <button type="button" className="login" name="login">Log in</button>
+        </div>
+    
+     
+         </div>
+      
+  
+        
+    );
+  }
+  
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ReactDOM.render(
+      <App />,
+    document.getElementById('root'));
